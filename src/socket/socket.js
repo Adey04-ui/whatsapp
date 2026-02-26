ce6e25a8418b4fcfadd7ce1a1f112451bc3867af
Kenny Odukoya <kennyodukoya001@gmail.com>
2026-02-26 20:16:47 +0100

    fixed socket connection url

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client"
 import { BASE_URI } from "../config/backend"
 
-const BASE_URL = "https://whatsapp-backend-tamk.onrender.com"
+const BASE_URL = "https://mock-backend-mjwh.onrender.com/"
 export const socket = io(BASE_URL, {
   autoConnect: false, 
   withCredentials: true,
